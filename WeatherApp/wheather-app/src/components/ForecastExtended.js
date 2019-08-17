6448c50a9d4ad5d3022a57c97a72ac7e504ed3c3
daniell21 <daniellorente20@gmail.com>
2019-08-17 16:23:12 +0100

    ultimos cambios seccion 11

--- WeatherApp/wheather-app/src/components/ForecastExtended.js
+++ WeatherApp/wheather-app/src/components/ForecastExtended.js
@@ -34,7 +34,18 @@ class ForecastExtended extends Component{
 
     componentDidMount() {
         //fetch or axios
-        const url_forecast = `${url}?q=${this.props.city}&appid=${api_key}`;
+       this.updateCity(this.props.city);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.city !== this.props.city){
+            this.setState({forecastData: null});
+            this.updateCity(nextProps.city);
+        }
+    }
+    
+    updateCity = city => {
+        const url_forecast = `${url}?q=${city}&appid=${api_key}`;
         
         //fetch: Hace invocacion al servidor
         fetch(url_forecast).then(
@@ -43,14 +54,20 @@ class ForecastExtended extends Component{
             weather_data => {
                 console.log(weather_data);
                 const forecastData = transformForecast(weather_data);
+                console.log(forecastData);
                 this.setState({forecastData});
             }
         );
     }
 
-    renderForecastItemDays(){
-        return <h1>"Render Items"</h1>;
-        //return days.map(day => (<ForecastItem weekDay={day} hour={10} data={data}></ForecastItem>));
+    renderForecastItemDays(forecastData){
+        return forecastData.map(forecastData => (
+        <ForecastItem 
+            key={`${forecastData.weekDay}${forecastData.hour}`}
+            weekDay={forecastData.weekDay} 
+            hour={forecastData.hour} 
+            data={forecastData.data}>
+        </ForecastItem>));
         
     }
 
@@ -67,7 +84,7 @@ class ForecastExtended extends Component{
         <div>
             <h2 className='forecast-title'>Pronóstico Extendido para {city} </h2>
             { forecastData ? 
-                this.renderForecastItemDays() : 
+                this.renderForecastItemDays(forecastData) : 
                 this.renderProgress()}
         </div>);
     }
